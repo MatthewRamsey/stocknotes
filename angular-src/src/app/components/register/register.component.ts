bb1f379fb42e4ec821a6c59a961dc00d4c431e84
Matthew Ramsey <ramsey.matthew@live.com>
2017-10-01 16:22:43 -0400

    added registration form and validation

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(private validateService: ValidateService,
+  private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Required fields
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout:3000})
+      return false;
+    }
+
+    // Validate email
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessage.show('Please enter a valid email', {cssClass: 'alert-danger', timeout:3000})
+      return false;
+    }
+  }
 }
